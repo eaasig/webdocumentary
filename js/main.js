6ce098230d761eeb6d11f1d6c54ac00fa709af10
eaasig <42800150+eaasig@users.noreply.github.com>
2019-02-17 13:55:37 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,160 @@
+
+/* Naming conventions
+section: sectionName
+variable: buttonSectionName
+function variable: imHiddenSectionName
+
+
+*/
+
+
+//First Steps
+var buttonFirstSteps = document.getElementById('buttonFirstSteps');
+
+buttonFirstSteps.addEventListener("click", function() {
+    
+    var imHiddenFirstSteps = document.getElementById('firstSteps');
+   
+    if (imHiddenFirstSteps.style.display == 'block') {
+        imHiddenFirstSteps.style.display = 'none'
+    } 
+    
+    else {
+        imHiddenFirstSteps.style.display = 'block'
+    }
+    
+    
+    
+}  )
+
+var buttonFirstStepsClose = document.getElementById('buttonFirstStepsClose');
+
+buttonFirstStepsClose.addEventListener("click", function() {
+    
+    var imHiddenFirstSteps = document.getElementById('firstSteps');
+   
+    if (imHiddenFirstSteps.style.display == 'block') {
+        imHiddenFirstSteps.style.display = 'none'
+    } 
+    
+      
+}  )
+
+
+//The Strings Sisters
+var buttonTheStringsSisters = document.getElementById('buttonTheStringsSisters');
+
+buttonTheStringsSisters.addEventListener("click", function() {
+    
+    var imHiddenTheStringsSisters = document.getElementById('theStringsSisters');
+   
+    if (imHiddenTheStringsSisters.style.display == 'block') {
+        imHiddenTheStringsSisters.style.display = 'none'
+    } 
+    
+    else {
+        imHiddenTheStringsSisters.style.display = 'block'
+    }
+  
+    
+}  )
+
+
+var buttonTheStringsSistersClose = document.getElementById('buttonTheStringsSistersClose');
+
+buttonTheStringsSistersClose.addEventListener("click", function() {
+    
+    var imHiddenFirstSteps = document.getElementById('theStringsSisters');
+   
+    if (imHiddenFirstSteps.style.display == 'block') {
+        imHiddenFirstSteps.style.display = 'none'
+    } 
+    
+      
+}  )
+
+//BACK TO MUSIC
+
+var buttonBackToMusic = document.getElementById('buttonBackToMusic');
+
+buttonBackToMusic.addEventListener("click", function() {
+    
+    var imHiddenBackToMusic = document.getElementById('backToMusic');
+   
+    if (imHiddenBackToMusic.style.display == 'block') {
+        imHiddenBackToMusic.style.display = 'none'
+    } 
+    
+    else {
+        imHiddenBackToMusic.style.display = 'block'
+    }
+    
+    
+    
+}  )
+
+var buttonFirstStepsClose = document.getElementById('buttonFirstStepsClose');
+
+buttonFirstStepsClose.addEventListener("click", function() {
+    
+    var imHiddenFirstSteps = document.getElementById('firstSteps');
+   
+    if (imHiddenFirstSteps.style.display == 'block') {
+        imHiddenFirstSteps.style.display = 'none'
+    } 
+    
+      
+}  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
